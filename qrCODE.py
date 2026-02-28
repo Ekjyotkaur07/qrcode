bc4c27bc9783be063572f23b8ab01ad1cc8b2b92
Ekjyot Kaur Malhotra <ekjyotkaurmalhotra@gmail.com>
2026-02-28 10:28:28 +0530

    Add files via upload

---
+++ qrCODE.py
@@ -0,0 +1,24 @@
+import qrcode
+
+def gen_qr():
+    print("QR CODE GENERATOR")
+
+    data = input("Enter data: ")
+    filename = input("Enter filename: ")
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img.save(filename + ".png")
+
+    print("QR Code Generated Successfully")
+
+gen_qr()
